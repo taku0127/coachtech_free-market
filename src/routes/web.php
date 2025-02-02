62ff668bec0e8bf691f61403399f47512683dad3
taku_y <y.taku84@gmail.com>
2025-02-03 00:27:42 +0900

    会員ページのログインミドルウェア設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,27 +12,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/sell', function () {
-    return view('sell');
-});
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/mypage/profile', function () {
-    return view('profile.setting');
-});
-Route::get('/mypage', function () {
-    return view('profile.index');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/mypage', function () {
+        return view('profile.index');
+    });
+    Route::get('/mypage/profile', function () {
+        return view('profile.setting');
+    });
+    Route::get('/sell', function () {
+        return view('sell');
+    });
+    Route::get('/purchase/{item}', function () {
+        return view('purchase');
+    });
+    Route::get('/purchase/address/{item}', function () {
+        return view('shipment');
+    });
 });
 Route::get('/item/{item}', function () {
     return view('detail');
-});
-Route::get('/item/{item}', function () {
-    return view('detail');
-});
-Route::get('/purchase/{item}', function () {
-    return view('purchase');
-});
-Route::get('/purchase/address/{item}', function () {
-    return view('shipment');
 });
